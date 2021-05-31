8745e113dc45de039b1393d21832f4e533541778
Ibrahima Niang <in2190@columbia.edu>
2021-05-31 19:29:09 -0400

    First commit with Spring dependecies

---
+++ src/main/java/com/outerlyy/outerlyyuserservice/OuterlyyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.outerlyy.outerlyyuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuterlyyUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuterlyyUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outerlyy/outerlyyuserservice/OuterlyyUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outerlyy.outerlyyuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterlyyUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
